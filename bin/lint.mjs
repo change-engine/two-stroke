e3dcb413d4b248e41ab09679c21796ed0bd81e45
Barnaby Shearer <barnaby@changeengine.com>
2025-06-24 17:07:47 +0100

    Enable lint cache for large projects

--- bin/lint.mjs
+++ bin/lint.mjs
@@ -2,5 +2,5 @@
 "use strict";
 import { cmd } from "../src/cmd.mjs";
 
-cmd("eslint --max-warnings=0");
-cmd("prettier --check .");
+cmd("eslint --cache --max-warnings=0");
+cmd("prettier --cache --check .");
