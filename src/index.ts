db6e1de6dbc08df0770e492d514944164d7847b9
Barnaby Shearer <barnaby@changeengine.com>
2026-07-21 17:40:40 +0100

    Vary: origin

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,7 @@ export function twoStroke<T>(
       context: ExecutionContext,
     ): Promise<Response> {
       const defaultHeaders = {
+        Very: "Origin",
         "Access-Control-Allow-Origin": origin
           ? (env.SENTRY_ENVIRONMENT === "staging" || env.SENTRY_ENVIRONMENT === "dev") &&
             req.headers.get("Origin")?.split(":")[1]?.endsWith("localhost")
@@ -101,9 +102,9 @@ export function twoStroke<T>(
                 rawBody = route.input
                   ? req.headers.get("Content-Type") === "application/x-www-form-urlencoded"
                     ? // oxlint-disable-next-line no-await-in-loop
-                      Object.fromEntries(new URLSearchParams(await req.text()))
+                    Object.fromEntries(new URLSearchParams(await req.text()))
                     : // oxlint-disable-next-line no-await-in-loop
-                      await req.json()
+                    await req.json()
                   : undefined;
               } catch (error) {
                 console.error({
@@ -124,10 +125,10 @@ export function twoStroke<T>(
               const body = route.input
                 ? route.input.safeParse(rawBody)
                 : {
-                    success: true,
-                    data: undefined,
-                    error: undefined,
-                  };
+                  success: true,
+                  data: undefined,
+                  error: undefined,
+                };
               if (body.success)
                 // oxlint-disable-next-line no-await-in-loop
                 response = await route.handler({
@@ -297,41 +298,41 @@ export function twoStroke<T>(
     noAuth,
     pbkdf:
       (k: keyof T, customHeaderName = "Authorization") =>
-      async ({ req, env }: { req: Request; env: T }) => {
-        const [scheme, token] = (req.headers.get(customHeaderName) ?? " ").split(" ");
-        if (
-          (scheme === "token" || scheme === "Bearer") &&
-          // oxlint-disable-next-line typescript/no-unsafe-type-assertion
-          (await pbkdfVerify(env[k] as string, token ?? ""))
-        )
-          return;
-        throw new Error("Invalid");
-      },
+        async ({ req, env }: { req: Request; env: T }) => {
+          const [scheme, token] = (req.headers.get(customHeaderName) ?? " ").split(" ");
+          if (
+            (scheme === "token" || scheme === "Bearer") &&
+            // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+            (await pbkdfVerify(env[k] as string, token ?? ""))
+          )
+            return;
+          throw new Error("Invalid");
+        },
     jwt:
       <J>(k: keyof T, ak: keyof T) =>
-      async ({ req, env }: { req: Request; env: T }) => {
-        const [scheme, token] = (req.headers.get("Authorization") ?? " ").split(" ");
-        if (scheme === "Bearer") {
-          // oxlint-disable-next-line typescript/no-unsafe-type-assertion
-          const rawIssuer = env[k] as string;
-          // oxlint-disable-next-line typescript/no-unsafe-type-assertion
-          const rawAudience = env[ak] as string;
-          const issuer = rawIssuer.startsWith("{")
-            ? // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+        async ({ req, env }: { req: Request; env: T }) => {
+          const [scheme, token] = (req.headers.get("Authorization") ?? " ").split(" ");
+          if (scheme === "Bearer") {
+            // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+            const rawIssuer = env[k] as string;
+            // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+            const rawAudience = env[ak] as string;
+            const issuer = rawIssuer.startsWith("{")
+              ? // oxlint-disable-next-line typescript/no-unsafe-type-assertion
               (JSON.parse(rawIssuer) as Record<string, JSONWebKeySet | true>)
-            : { [rawIssuer]: true as const };
-          const audience = rawAudience.startsWith("[")
-            ? // oxlint-disable-next-line typescript/no-unsafe-type-assertion
+              : { [rawIssuer]: true as const };
+            const audience = rawAudience.startsWith("[")
+              ? // oxlint-disable-next-line typescript/no-unsafe-type-assertion
               (JSON.parse(rawAudience) as string[])
-            : [rawAudience];
-          const claims = await jwkVerifyMulti<J>(token ?? "", issuer, audience);
-          if (!claims) {
-            throw new Error("Invalid");
+              : [rawAudience];
+            const claims = await jwkVerifyMulti<J>(token ?? "", issuer, audience);
+            if (!claims) {
+              throw new Error("Invalid");
+            }
+            return claims;
           }
-          return claims;
-        }
-        throw new Error("Invalid");
-      },
+          throw new Error("Invalid");
+        },
     queueHandler<I extends ZodType>(
       input: I,
       handler: (c: {
